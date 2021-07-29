4f8a3129a01eeb73168bb66c91352d09dc589b9c
Zemlyar Igor <zemlyar2002@icloud.com>
2021-07-29 14:52:06 +0300

    first

---
+++ Facebook/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Facebook.Controllers
+{
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        [Route("signin")]
+        public IActionResult SignIn()
+        {
+            return Challenge(new AuthenticationProperties{RedirectUri="/"});
+        }
+    }
+}
